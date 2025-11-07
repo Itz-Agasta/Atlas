ab3784c62406ec27e700b4e0bde5ff28adf805e6
Itz-Agasta <rupamgolui69@gmail.com>
2025-11-07 15:29:04 +0530

    minor fixs

--- apps/workers/src/atlas_workers/__init__.py
+++ apps/workers/src/atlas_workers/__init__.py
@@ -1,7 +1,7 @@
 """ARGO Float Data Processing Workers."""
 
 __version__ = "0.1.0"
-__author__ = "Atlas Team"
+__author__ = "Team Vyse"
 
 from .models import FloatMetadata, ProfileData
 from .workers import FTPSyncWorker, NetCDFParserWorker
