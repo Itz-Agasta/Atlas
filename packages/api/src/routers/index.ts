46ca29c82d54dd2e913d839755d29b38f2501b37
Itz-Agasta <rupamgolui69@gmail.com>
2025-11-21 05:41:55 +0530

    fea (tRPC)t: Add agent router and input schemas

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,6 +1,8 @@
 import { publicProcedure, router } from "../index";
+import { agentRouter } from "./v1/agent";
 
 export const appRouter = router({
   healthCheck: publicProcedure.query(() => "OK"),
+  agent: agentRouter,
 });
 export type AppRouter = typeof appRouter;
