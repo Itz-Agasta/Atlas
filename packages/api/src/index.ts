01e471d5da6e014f3002b5d19e992386ff7ba43a
Itz-Agasta <rupamgolui69@gmail.com>
2026-01-09 20:03:26 +0530

    Remove the trpi api pakage

--- packages/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Export shared domain types (explicit exports to avoid barrel file issues)
-export type {
-  Citation,
-  DataQuality,
-  ResearchPaperChunk,
-  ScientificResponse,
-} from "./types";
